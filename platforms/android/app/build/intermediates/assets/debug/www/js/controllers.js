ab74034afd0451de7e074328813207b37aa780ae
qiangzh89 <qiangzh89@gmail.com>
2018-03-25 21:40:05 -0400

    first commit

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/controllers.js
@@ -0,0 +1,113 @@
+angular.module('starter.controllers', ['starter.services'])
+
+.controller('DashCtrl', function($scope, $state, $rootScope) {
+  $scope.operation = function(operation){
+    $rootScope.operationSelected = operation;
+    $state.go('tab.quizzes');
+  };
+})
+
+.controller('QuizzesCtrl', function($scope, $rootScope, Scores, $state) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  $scope.$on('$ionicView.enter', function(e) {
+    
+  });
+  var result;
+  $scope.newscore = {};
+  $scope.newscore.level = 1;
+  $scope.numOne = Math.floor(Math.random() * 10 + 1);
+  $scope.numTwo = Math.floor(Math.random() * 10 + 1);
+  if ($rootScope.operationSelected == 'Addition') {
+    result = $scope.numOne + $scope.numTwo;
+    $scope.operate = '+';
+  } else if ($rootScope.operationSelected == 'Subtraction') {
+    result = $scope.numOne - $scope.numTwo;
+    $scope.operate = '-';
+  } else if ($rootScope.operationSelected == 'Multiplication') {
+    result = $scope.numOne * $scope.numTwo;
+    $scope.operate = '*';
+  } else {
+    result = $scope.numOne % $scope.numTwo;
+    $scope.operate = '%';
+  }
+  $scope.guesses = 0;
+  $scope.correctAns = 0;
+  $scope.newscore.score = $scope.correctAns + ' / ' + $scope.guesses;
+
+  var img = document.getElementById("rightOrWrong");
+  $scope.checkAnswer = function() {
+    $scope.guesses++;
+    if ($rootScope.operationSelected == 'Addition') {
+      result = $scope.numOne + $scope.numTwo;
+      $scope.operate = '+';
+    } else if ($rootScope.operationSelected == 'Subtraction') {
+      result = $scope.numOne - $scope.numTwo;
+      $scope.operate = '-';
+    } else if ($rootScope.operationSelected == 'Multiplication') {
+      result = $scope.numOne * $scope.numTwo;
+      $scope.operate = '*';
+    } else {
+      result = $scope.numOne % $scope.numTwo;
+      $scope.operate = '%';
+    }
+    if ($scope.guesses >= 10) {
+      $scope.newscore.score = (result == $scope.inputAnswer ? 1 + $scope.correctAns : $scope.correctAns) + ' / 10';
+      Scores.add($scope.newscore);
+
+      var newLevel;
+      if ($scope.correctAns < 6) {
+        newLevel = $scope.newscore.level;
+        $scope.message = "You failed this level. Please try again!";
+        var audio = new Audio("audio/fail.wav");
+        audio.play();
+      } else {
+        $scope.message = "Congratulations you passed level " + $scope.newscore.level + ".";
+        var audio = new Audio("audio/success.wav");
+        audio.play();
+        newLevel = $scope.newscore.level + 1;
+      }
+
+      $scope.newscore = {};
+      $scope.newscore.level = newLevel;
+      if ($scope.newscore.level >= 4) {
+        $rootScope.congratMsg = "Congratulations! You passed all levels in operation " + $scope.operationSelected + "!";
+        $state.go('tab.score');
+        $scope.newscore.level = 1;
+        img.src = "img/thumb.png";
+        $scope.inputAnswer = "";
+        $scope.message = "";
+      } 
+      $scope.newscore.score = '0 / 0';
+      $scope.numOne = Math.floor(Math.random() * Math.pow(10, $scope.newscore.level) + 1);
+      $scope.numTwo = Math.floor(Math.random() * Math.pow(10, $scope.newscore.level) + 1);
+      $scope.guesses = 0;
+      $scope.correctAns = 0;        
+    } else {
+      if ($scope.guesses >= 1) {
+        $scope.message = "";
+      }
+      
+      if (result == $scope.inputAnswer) {
+        $scope.correctAns++;
+        img.src = 'img/Right.png';
+      } else {
+        img.src = 'img/Wrong.png';
+      }
+      $scope.newscore.score = $scope.correctAns + ' / ' + $scope.guesses;
+      $scope.numOne = Math.floor(Math.random() * Math.pow(10, $scope.newscore.level) + 1);
+      $scope.numTwo = Math.floor(Math.random() * Math.pow(10, $scope.newscore.level) + 1);
+    }
+  };
+})
+
+.controller('ScoreCtrl', function($scope, Scores) {
+  $scope.$on('$ionicView.enter', function(e) {
+    $scope.scores = Scores.all();
+  });
+});
---
+++ platforms/android/app/src/main/assets/www/js/services.js
@@ -0,0 +1,21 @@
+angular.module('starter.services', [])
+
+.service('Scores', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var scores = JSON.parse(window.localStorage.getItem('scores'));
+  if (scores == null) {
+    scores = [];
+  }
+
+  this.all = function() {
+    return scores;
+  };
+
+  this.add = function(score) {
+    score.time = (new Date()).toString();
+    scores.push(score);
+    window.localStorage.setItem('scores', JSON.stringify(scores));
+  };
+});
